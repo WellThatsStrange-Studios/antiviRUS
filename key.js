85321cfb26b51355e465dad0b0a6548559f580fe
danik the not <69620736+notdanik@users.noreply.github.com>
2021-01-27 14:19:38 +0100

    Delete key.js

--- key.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-const Crypto = require('crypto')
-
-if (Crypto.createHash('sha256').update(fs.readFileSync(process.argv[2] + '/antiviRUS.key')).digest('base64') === 'UdIX5LYn7eTR7PPU0bKJcIpER9rwvfqBml17USgk0P8=') {
-  process.exit(0)
-} else {
-  process.exit(1)
-}
